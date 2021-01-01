a9c693b39c08a797781a5f8287cfc8d19a8284d1
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2021-01-01 23:21:51 +0100

    set pusher

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const port = process.env.PORT || 4000
 app.use(express.json())
 app.use(cors())
 
+const pusher = new Pusher({
+    appId: "1131397",
+    key: "93df24426ba9d39491dc",
+    secret: "4adee0d4decabc1106c5",
+    cluster: "eu",
+    useTLS: true
+});
+
 //db config
 const mongoURI ='mongodb+srv://admin:GNyA0YEbIXHjBa4D@cluster0.xcabl.mongodb.net/messengerDB?retryWrites=true&w=majority'
 
@@ -26,6 +34,12 @@ mongoose.connect(mongoURI, {
 //to check if connection is working
 mongoose.connection.once('open', () => {
     console.log('db connected')
+    const changeStream = mongoose.connection.collection('messages').watch()
+    changeStream.on('change', (change) => {
+        pusher.trigger('messages', "newMessage", {
+            'change': change
+        });
+    })
 })
 
 //api routes
